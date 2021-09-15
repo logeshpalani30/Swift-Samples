91e5f994ccf59860b652705a084c52ade2e2991b
Logesh Palani <logeshp@softura.com>
2021-09-15 09:58:25 +0530

    updated all code

---
+++ MapView/MapView/Captial.swift
@@ -0,0 +1,21 @@
+//
+//  Captial.swift
+//  MapView
+//
+//  Created by Logesh Palani on 08/09/21.
+//
+
+import UIKit
+import MapKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
